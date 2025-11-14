3e7115f42273edff9ab3cbcd9d67deb45b6e52db
erenworld <erenxturkoglu@gmail.com>
2025-11-14 07:35:14 +0100

    Add the worker struct

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,22 @@
 package worker
+
+import (
+	"orkestra/task"
+
+	"github.com/google/uuid"
+	"github.com/golang-collections/collections/queue"
+) 
+
+// Run tasks as Docker containers
+// Accept tasks to run from a manager
+// Provide relevant statistics to the manager for the purpose of scheduling tasks
+// Keep track of its tasks and their state
+
+// TODOS: implement my own queue in Golang
+// TODOS: implement my own DB
+type Worker struct {
+	Name 		string
+	Queue   	queue.Queue					// a map of UUIDs to tasks
+	Db			map[uuid.UUID]*task.Task   	// Tasks are handled in FIFO order  
+	TaskCount 	int
+}
